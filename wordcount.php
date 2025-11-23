582d73add2c8435455896d001b7425cd729d3a8a
code-with-akshay3 <manjareakshay48@gmail.com>
2025-11-22 23:23:46 -0800

    Add files via upload

---
+++ wordcount.php
@@ -0,0 +1,10 @@
+<html> 
+      <body>
+            <?php
+                 $string = "simple code of a string";
+                 $wordcount = str_word_count($string);
+
+                 echo"the total number of word in the string is $wordcount";
+            ?>
+      </body>
+</html>
